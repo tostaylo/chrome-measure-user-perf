1c4cc79a53f0201cf361817d432c398b7eb0e44a
Torre <tostaylo1@gmail.com>
2020-12-25 12:49:27 -0600

    getting total duration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-"use strict";
+'use strict';
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 (async () => {
     try {
         const data_click_vals = await getInteractiveElements();
@@ -16,6 +17,7 @@ const puppeteer = require('puppeteer');
             await browser.close();
             console.log('closing browser');
         }
+        processFiles();
         process.exit(0);
     }
     catch (err) {
@@ -45,4 +47,58 @@ async function getInteractiveElements() {
     });
     browser.close();
     return data_click_vals;
+}
+function processFiles() {
+    console.log('Reading Traces Directory');
+    fs.readdirSync('../traces/').forEach((file) => {
+        var _a;
+        const path = `../traces/${file}`;
+        const fileName = file.split('.')[1];
+        console.log(fileName);
+        try {
+            const data = fs.readFileSync(path, 'utf8');
+            const json = JSON.parse(data);
+            let finalCompositeStartTime = 0;
+            let finalCompositeDur = 0;
+            let clickStartTime = 0;
+            let clickDur = 0;
+            (_a = json === null || json === void 0 ? void 0 : json.traceEvents) === null || _a === void 0 ? void 0 : _a.forEach((entry) => {
+                var _a, _b;
+                if (((_b = (_a = entry.args) === null || _a === void 0 ? void 0 : _a.data) === null || _b === void 0 ? void 0 : _b.type) === RenderEvent.Click) {
+                    clickStartTime = entry.ts;
+                    clickDur = entry.dur;
+                }
+                if (isCompositeEvent(entry.name)) {
+                    if (entry.ts > finalCompositeStartTime) {
+                        finalCompositeStartTime = entry.ts;
+                        finalCompositeDur = entry.dur;
+                    }
+                }
+            });
+            const totalDur = finalCompositeStartTime + finalCompositeDur - clickStartTime;
+            console.log(totalDur);
+        }
+        catch (err) {
+            console.error(err);
+        }
+        // try {
+        // 	fs.unlinkSync(`../traces/${file}`);
+        // } catch (err) {
+        // 	console.error(err);
+        // }
+    });
+}
+var RenderEvent;
+(function (RenderEvent) {
+    RenderEvent["Click"] = "click";
+    RenderEvent["Layout"] = "Layout";
+    RenderEvent["UpdateLayoutTree"] = "UpdateLayoutTree";
+    RenderEvent["Paint"] = "Paint";
+    RenderEvent["CompositeLayers"] = "CompositeLayers";
+})(RenderEvent || (RenderEvent = {}));
+function isCompositeEvent(event) {
+    if (event === RenderEvent.CompositeLayers) {
+        return true;
+    }
+    return false;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+'use strict';
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
 	try {
@@ -19,6 +21,8 @@ const puppeteer = require('puppeteer');
 			await browser.close();
 			console.log('closing browser');
 		}
+
+		processFiles();
 
 		process.exit(0);
 	} catch (err) {
@@ -55,4 +59,69 @@ async function getInteractiveElements(): Promise<string[]> {
 
 	browser.close();
 	return data_click_vals;
+}
+
+function processFiles() {
+	console.log('Reading Traces Directory');
+	fs.readdirSync('../traces/').forEach((file: string) => {
+		const path = `../traces/${file}`;
+		const fileName = file.split('.')[1];
+		console.log(fileName);
+
+		try {
+			const data = fs.readFileSync(path, 'utf8');
+			const json = JSON.parse(data);
+
+			let finalCompositeStartTime = 0;
+			let finalCompositeDur = 0;
+			let clickStartTime = 0;
+			let clickDur = 0;
+
+			json?.traceEvents?.forEach((entry: TraceEntry) => {
+				if (entry.args?.data?.type === RenderEvent.Click) {
+					clickStartTime = entry.ts;
+					clickDur = entry.dur;
+				}
+
+				if (isCompositeEvent(entry.name)) {
+					if (entry.ts > finalCompositeStartTime) {
+						finalCompositeStartTime = entry.ts;
+						finalCompositeDur = entry.dur;
+					}
+				}
+			});
+			const totalDur = finalCompositeStartTime + finalCompositeDur - clickStartTime;
+			console.log(totalDur);
+		} catch (err) {
+			console.error(err);
+		}
+
+		// try {
+		// 	fs.unlinkSync(`../traces/${file}`);
+		// } catch (err) {
+		// 	console.error(err);
+		// }
+	});
+}
+
+interface TraceEntry {
+	args: { data: { type: string } };
+	name: string;
+	ts: number;
+	dur: number;
+}
+
+enum RenderEvent {
+	Click = 'click',
+	Layout = 'Layout',
+	UpdateLayoutTree = 'UpdateLayoutTree',
+	Paint = 'Paint',
+	CompositeLayers = 'CompositeLayers',
+}
+
+function isCompositeEvent(event: string): boolean {
+	if (event === RenderEvent.CompositeLayers) {
+		return true;
+	}
+	return false;
 }
